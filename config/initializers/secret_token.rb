2e1ddf151be57ec72600fd1cf143c44a08551f71
shakil <mehedi_nsu07@hotmail.com>
2013-11-18 01:20:10 +0600

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Chotolok::Application.config.secret_token = '2cfbe080088f5306763aa17c5d68d45bc8454afbd96b63110dd74d4f0d5d94b0b85946b4a90bf2386800b5e47747f5820e12097634d2ad3d56336606151c567f'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+Chotolok::Application.config.session_store :cookie_store, key: '_chotolok_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rails generate session_migration")
+# Chotolok::Application.config.session_store :active_record_store
