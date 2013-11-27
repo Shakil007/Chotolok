d7bece97f09bdee7f0aae6547803d673b600756f
shakil <mehedi_nsu07@hotmail.com>
2013-11-27 13:39:20 +0600

    Change to Ruby 1.9-style hash syntax

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -10,10 +10,15 @@ describe "Static pages" do
       page.should have_selector('h1', :text => 'Home')
     end
 
-    it "should have the right title" do
+    it "should have the base title" do
       visit '/static_pages/home'
-       page should have_selector('title',:text => "Chotolok | Home")
+       page should have_selector('title',:text => "Chotolok ")
     end
+
+    it "should not have a custom page title"
+       visit '/static_pages/home'
+       page.should_not have_selector('title', :text => '| Home')
+     end
    end
 
   describe "Help page" do
