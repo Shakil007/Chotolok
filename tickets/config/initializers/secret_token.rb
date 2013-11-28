c3a97dc2a4118789ffe2bc18e7fe47284734b9b9
shakil <mehedi_nsu07@hotmail.com>
2013-11-28 09:26:54 +0600

    Make a basic User model

---
+++ tickets/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Tickets::Application.config.secret_token = 'ed93d652bce6983e3282a4eb2d4924bcae16d37ef3dec45d7fb644ea0c43017bfbe7c25af26fb30935e72b9cdfa77eebae5c05b9b4b814cc0beff42d702f5c0e'
