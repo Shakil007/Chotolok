70123a05ceca9732b6fcc893557a550ca8bf24dd
shakil <mehedi_nsu07@hotmail.com>
2013-12-07 17:49:30 +0600

    Add Cucumber features

--- spec/requests/authentication_pages_spec.rb
+++ spec/requests/authentication_pages_spec.rb
@@ -19,15 +19,15 @@ describe "AuthenticationPages" do
       before { click_button "Sign in" }
 
       it { should have_selector('title', text: 'Sign in') }
-      it { should have_selector('div.alert.alert-error', text: 'Invalid') }
+      it { should have_error_message }
 
       describe "after visiting another page" do
         before { click_link "Home"}
-        it { should_not have_selector('div.alert.alert-error') }
+        it { should_not have_error_message }
 
       end
     end
-  end
+
 
   describe "with valid information" do
     let(:user) { FactoryGirl.create(:user) }
